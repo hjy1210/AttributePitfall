74916c3b0a968d6375edec899ef695791e12908d
hjyanghj <hjyanghj@gmail.com>
2021-10-03 09:21:10 +0800

    add a project demostrate pitfall of add attribute in Blazor WebAssembly app

---
+++ AttributePitfall/Models/MyModel.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AttributePitfall.Models
+{
+    public class MyModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
